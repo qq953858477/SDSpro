17ae3cf889ff998c4a371b6bc89f3a4283ba1691
qq953858477 <953858477@qq.com>
2016-01-04 14:35:46 +0800

    1111

--- app/src/main/java/com/xinyusoft/sdspro/fragment/StockFragment.java
+++ app/src/main/java/com/xinyusoft/sdspro/fragment/StockFragment.java
@@ -62,7 +62,6 @@ public class StockFragment extends Fragment implements OnClickListener {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragement_stockinfo, container, false);
 
-
         initView();
         getZXGActionInfo();
         createSwipemenulistview();

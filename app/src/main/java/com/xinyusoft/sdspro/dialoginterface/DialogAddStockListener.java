40e0dc0fbb6f509df26651448dfe8fd4a599948c
qq953858477 <953858477@qq.com>
2016-01-08 10:58:56 +0800

    zc

---
+++ app/src/main/java/com/xinyusoft/sdspro/dialoginterface/DialogAddStockListener.java
@@ -0,0 +1,10 @@
+package com.xinyusoft.sdspro.dialoginterface;
+
+import com.xinyusoft.sdspro.bean.Stock;
+
+/**
+ * Created by ZHOUCHAO on 2016/1/4.
+ */
+public interface DialogAddStockListener {
+    public void add(Stock stock);
+}
